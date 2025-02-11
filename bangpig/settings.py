3dec74c4dc4e0379713814d061b074f9ca204164
trebmal <bikoyemmanuel531@gmail.com>
2025-02-11 15:36:01 +0100

    setting update

--- bangpig/settings.py
+++ bangpig/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS=['bangpig.onrender.com', '127.0.0.1', 'localhost', 'bangpig.com', 'bangpig.com/', 'https://www.bangpig.com/', 'https://www.bangpig.com', 'www.bangpig.com/']
 
 
 # Application definition
